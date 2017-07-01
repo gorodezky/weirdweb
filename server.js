82d1f6ba8b20ed3097b24bd87ba7acd74b2cde82
Dan Gorodezky <dan.gorodezky@yale.edu>
2017-07-01 10:55:55 -0700

    trying to work on heroku

--- server.js
+++ server.js
@@ -4,17 +4,6 @@ const Hapi = require('hapi');
 const Path = require('path');
 const Nunjucks = require('nunjucks');
 
-
-// var express = require('express');
-// var app = express();
-
-// // prepare server
-// app.use('/', express.static(__dirname + '/www')); // redirect root
-// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS boots
-
-
 const controllers = {};
 controllers.index = require('./controllers/index.js');
 
@@ -64,6 +53,7 @@ server.route({
         return reply('Hello world. Works.');
     }
 });
+
 server.route({
     method: 'GET',
     path:'/', 
